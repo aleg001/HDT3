08c669b18278620647634be79b2ffd8489f827f1
birdpearl <77627803+birdpearl@users.noreply.github.com>
2021-02-14 23:36:09 -0600

    final radix

--- RadixSort.java
+++ RadixSort.java
@@ -1,32 +1,38 @@
-import java.util.Arrays;
-
-
-/*******************************************************
- * Universidad del Valle de Guatemala Algoritmos y Estructuras de Datos
- * Profesor: Moises Gonzales RadixSort.java Autores: Alejandro Gomez y Ana
- * Ramirez Fecha de creacion: 09/02/2021 Ultima edicion: 10/02/2021
- ********************************************************/
+package colocarr;
+import java.util.*;
 
 public class RadixSort {
-
-    
-      /**
-      * Método para proceso de Radix
-      * 
-      * @param Comparable lista
-      * @return Comparable lista
-      * @author Alejandro Gomez
-      **/
-
-      /* Metodo para proceso de RadixSorter */
-    public static String RadixSorter(InterfaceComparar[] lista, int n)
-    {   //Se declaran variables
-        int RadixSorteador = getMax(lista, n);
-        //Ciclo for para cada uno de los valores
-        for (int xd = 1; RadixSorteador / xd > 0; xd *= 10) countSort(lista, n, xd);
-        return ("Sus datos en forma ordenada son: ");}
-
-
+	public RadixSort() {
+		
+	}
+	private int get_max_val(int my_arr[], int arr_len) {
+	      int max_val = my_arr[0];
+	      for (int i = 1; i < arr_len; i++)
+	         if (my_arr[i] > max_val)
+	         max_val = my_arr[i];
+	      return max_val;
+	}
+	private void countSort(int my_arr[], int arr_len, int exp) {
+	      int result[] = new int[arr_len];
+	      int i;
+	      int count[] = new int[10];
+	      Arrays.fill(count,0);
+	      for (i = 0; i < arr_len; i++)
+	         count[ (my_arr[i]/exp)%10 ]++;
+	      for (i = 1; i < 10; i++)
+	         count[i] += count[i - 1];
+	      for (i = arr_len - 1; i >= 0; i--) {
+	         result[count[ (my_arr[i]/exp)%10 ] - 1] = my_arr[i];
+	         count[ (my_arr[i]/exp)%10 ]--;
+	      }
+	      for (i = 0; i < arr_len; i++)
+	         my_arr[i] = result[i];
+	 }
+	 public int[] compareTo(int[] my_arr) {
+		  int arr_len=my_arr.length;
+	      int m = get_max_val(my_arr, arr_len);
+	      for (int exp = 1; m/exp > 0; exp *= 10)
+	      countSort(my_arr, arr_len, exp);
+	      return my_arr;
+	  }
 }
-
-
