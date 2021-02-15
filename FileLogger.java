b4b8407e6812d04878b000a7f456ac3d81cd9c6b
Ale Gómez <60187741+aleg001@users.noreply.github.com>
2021-02-14 22:30:16 -0600

    Se simplifco los metodos

--- FileLogger.java
+++ FileLogger.java
@@ -20,6 +20,7 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.io.FileWriter;
 
 public class FileLogger {
 	
@@ -42,11 +43,33 @@ public class FileLogger {
 	 * @throws IOException
 	 */
 	public static String CrearArchivo(String fileName) throws IOException{
-		File LoggingFile = new File(fileName + ".txt");
+		File LoggingFile = new File("datos.txt");
 		LoggingFile.createNewFile();
 		return fileName + ".txt";
 	}
+
+	public static void CreateArchivoTexto(){ 
+		try {
+			File data = new File("Datos.txt");
+			if (data.createNewFile()) {
+				System.out.println("Se creo archivo: " + data.getName());
+			} else {
+				System.out.println("Su archivo ya existe...");
+			}
+			} catch (IOException e) {
+			System.out.println("Cuidado! Ocurrio un error.");}
+		}
 	
+	public static void EscribirLinea(String newline){ 
+		try {
+			FileWriter escribirFile = new FileWriter("datos.txt");
+			escribirFile.write(newline);
+			escribirFile.close();
+		} catch (IOException e) {
+			System.out.println("Ocurrio un error...");
+		}
+		}
+			
 	/***
 	 * Agrega lineas nuevas de texto al archivo
 	 * @param fileName Donde ingresara
@@ -114,4 +137,63 @@ public class FileLogger {
 			return "Archivo no encontrado!";
 		}
 	}
+
+
+	
+	/***
+	 * CreadorDeNumerosEscritura
+
+	 * @param ArrayList<Integer> Numbers
+	 * @return n/a
+	 */
+
+	/*	Se crean numeros y genera numeros
+		
+	*/
+
+    public static void CreadorDeNumerosEscritura(ArrayList<Integer> Numbers){
+        Numbers.clear();
+        try{
+			//Se declaran instancias
+            Random rand = new Random();String Ubicacion = "datos.txt";String lines = "";StringBuilder CreadorDeStrings = new StringBuilder();
+            //Se declara ciclo for para generar 3000 numeros aleatorios
+            for(int i=0;i<3000;i++){
+                //Se declaran instancias
+				boolean CicloBooleano = true; int NumerosAleatorios = rand.nextInt(6000);
+                do{
+					//Se crea ciclo if para los numeros
+                    if(Numbers.contains(NumerosAleatorios)){
+                        NumerosAleatorios = rand.nextInt(6000);
+					//Condicion distinta
+                    }else{
+                        Numbers.add(NumerosAleatorios);
+                        CicloBooleano=false;
+                    }
+                }while(CicloBooleano);               
+            }
+            
+            //guardar los numeros en string
+            for(Integer Numerito : Numbers){
+                CreadorDeStrings.append(Numerito + " ");
+            }
+            lines = CreadorDeStrings.toString();
+
+            //Se almacena lo del archivo creado
+            File ArchivoCreado = new File(Ubicacion);
+            if(!ArchivoCreado.exists()){ArchivoCreado.createNewFile();}
+
+            //En el archivo se crea lo indicado
+            FileWriter EscrituraArchivo = new FileWriter(ArchivoCreado); BufferedWriter BufferEscritor = new BufferedWriter(EscrituraArchivo);
+
+            //Los archivos de texto se escriben y cierran
+            BufferEscritor.write(lines);BufferEscritor.close();
+
+			//En caso de un error
+        }catch(Exception e){
+            e.printStackTrace();
+			System.out.println("Algo salio mal...");
+        }
+    }
+
+
 }
