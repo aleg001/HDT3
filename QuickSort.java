670ff94fc9c63118c36af59d12b19b1ae2c42b4a
birdpearl <77627803+birdpearl@users.noreply.github.com>
2021-02-14 22:50:12 -0600

    final quick

--- QuickSort.java
+++ QuickSort.java
@@ -1,5 +1,6 @@
-
+package colocarr;
 /*******************************************************
+ /*******************************************************
  * Universidad del Valle de Guatemala 
  * Algoritmos y Estructuras de Datos
  * Profesor: Moises Gonzales 
@@ -9,13 +10,12 @@
  * Ultima edicion: 10/02/2021
  ********************************************************/
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 
 public class QuickSort  {
 
-  
+     public QuickSort() {
+    	 
+     }
     /**
      * Metodo QuickSorter
      * @param Comparable[] lista
@@ -23,14 +23,24 @@ public class QuickSort  {
      * @param Int alto
      * @author Alejandro Gomez y Ana Ramirez
      **/
+     
+     public int[] compareTo(int[] lista) {
+    	 int alto=lista.length-1;
+    	 int bajo=0;
+    	 QuickSorter(lista,bajo,alto);
+    	 return lista;
+     }
 
      /* Metodo para proceso de quicksort */
-      public static String QuickSorter(Comparable[] lista, int bajo, int alto)
+      private void QuickSorter(int[] lista, int bajo, int alto)
       //Se realiza un ciclo if en caso el dato alto es mas pequeno que el bajo.
-      {if (bajo < alto)
-          {int IndexParticion = MetodoParticion(lista, bajo, alto); QuickSorter(lista, bajo, IndexParticion-1); QuickSorter(lista, IndexParticion+1, alto);}
-          //Se manda mensaje que se ordeno datos
-          return ("Sus datos de forma ordenada son: ");}
+      {
+    	  if (bajo < alto){
+    		  int IndexParticion = MetodoParticion(lista, bajo, alto); 
+              QuickSorter(lista, bajo, IndexParticion-1); 
+              QuickSorter(lista, IndexParticion+1, alto);
+    	  }
+      }
 
 
       
@@ -44,15 +54,23 @@ public class QuickSort  {
 
      /* Metodo para proceso de particion */
 
-      private static int MetodoParticion(Comparable[] list, int bajo, int alto) {
+      private static int MetodoParticion(int[] list, int bajo, int alto) {
         //Se definen instancias
-        Comparable Pivote = list[alto]; int i = (bajo-1);
+        int Pivote = list[alto]; 
+        int i = (bajo-1);
         //Se crea ciclo for:
         for (int j=bajo; j<alto; j++)
         //Se crea ciclo if en la cual se compara el tamano del pivote.
-        {if (list[j].compareTo(Pivote) > 0) {i++; Comparable temp = list[i];list[i] = list[j];list[j] = temp;}}
+        {
+        	if (list[j] <= Pivote) {
+        		i++; 
+            	int temp = list[i];
+            	list[i] = list[j];
+            	list[j] = temp;        	
+        	}
+        }
         //Se define una instancia temporal
-        Comparable temp = list[i+1];
+        int temp = list[i+1];
         list[i+1] = list[alto];
         list[alto] = temp;
         //Se devuelve i+1.
@@ -60,6 +78,5 @@ public class QuickSort  {
     }
 
     
-}
-    
+} 
 
